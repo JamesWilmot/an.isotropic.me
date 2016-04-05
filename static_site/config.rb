d5e35bed5e0dbd80d04e325a926bde31dbde7fee
James Wilmot <jameswilmot2000@gmail.com>
2016-04-05 17:55:33 +1000

    Most basic journal content type

--- static_site/config.rb
+++ static_site/config.rb
@@ -28,7 +28,8 @@ activate :blog do |blog|
   # Matcher for blog source files
   blog.sources = "{year}-{month}-{day}-{title}.html"
   # blog.taglink = "tags/{tag}.html"
-  # blog.layout = "layout"
+  
+  blog.layout = "journal"
   # blog.summary_separator = /(READMORE)/
   # blog.summary_length = 250
   # blog.year_link = "{year}.html"
--- static_site/source/journal/2016-04-05-example-article.html.markdown
+++ static_site/source/journal/2016-04-05-example-article.html.markdown
@@ -2,6 +2,11 @@
 title: Example Article
 date: 2016-04-05
 tags: example
+author: Jimmy Wilmot
 ---
 
 This is an example article. You probably want to delete it and write your own articles!
+
+---
+
+After horizontal rule
